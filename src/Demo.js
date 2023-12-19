4762481fd96c3f0766637b60deb06737061e2555
HarshithaRaj13 <harshitharajshekar13@gmail.com>
2023-12-19 14:14:30 +0530

    first commit

---
+++ src/Demo.js
@@ -0,0 +1,21 @@
+function Demo() {
+  return (
+    <div>
+      const myelement = (
+      <table>
+        <tr>
+          <th>Name</th>
+        </tr>
+        <tr>
+          <td>John</td>
+        </tr>
+        <tr>
+          <td>Elsa</td>
+        </tr>
+      </table>
+      ); const container = document.getElementById('root'); const root =
+      ReactDOM.createRoot(container); root.render(myelement);
+    </div>
+  );
+}
+export default Demo;
